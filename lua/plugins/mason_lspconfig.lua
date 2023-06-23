178e4bc591779e96a3b6caa70e209630295aa513
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-06-23 07:49:57 -0500

    test

--- lua/plugins/mason_lspconfig.lua
+++ lua/plugins/mason_lspconfig.lua
@@ -2,6 +2,7 @@ return {
 	"williamboman/mason-lspconfig.nvim",
 	dependencies = {
 		"williamboman/mason.nvim",
+		"neovim/nvim-lspconfig",
 	},
 	config = function()
 		require("mason-lspconfig").setup()
