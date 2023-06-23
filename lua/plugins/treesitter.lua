f82a474a75e0091321d5ace8380e32deb8e5e344
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-06-23 09:10:39 -0500

    ts init

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,31 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"python",
+					"regex",
+					"bash",
+					"markdown",
+					"markdown_inline",
+				},
+				auto_install = true,
+				highlight = {
+					enable = true,
+				},
+				indent = {
+					enable = true,
+				},
+			})
+			vim.opt.foldlevel = 20
+			vim.opt.foldmethod = "expr"
+			vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+		end,
+		build = ":TSUpdate",
+	},
+}
