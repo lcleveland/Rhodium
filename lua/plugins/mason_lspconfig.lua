a72db3d7fe05fbb56aee5c9a8f368ab09d9c8ebb
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-06-23 07:44:38 -0500

    auto setup

--- lua/plugins/mason_lspconfig.lua
+++ lua/plugins/mason_lspconfig.lua
@@ -3,5 +3,12 @@ return {
 	dependencies = {
 		"williamboman/mason.nvim",
 	},
-	config = true,
+	config = function()
+		require("mason-lspconfig").setup()
+		require("mason-lspconfig").setup_handlers({
+			function(server_name)
+				require("lspconfig")[server_name].setup({})
+			end,
+		})
+	end,
 }
