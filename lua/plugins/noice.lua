b40e5f6cf5df7fa0a63a2ea8248facb5fb05611b
Lyle Cleveland <lcleveland@pop-os.localdomain>
2023-06-23 20:31:14 -0500

    switch to noice

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,23 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"rcarriga/nvim-notify",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("noice").setup({
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
+			presets = {
+				command_palette = true,
+			},
+		})
+		require("telescope").load_extension("noice")
+	end,
+}
