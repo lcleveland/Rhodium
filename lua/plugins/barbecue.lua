461efae1ec22e2dc7bb02439f1de6fd29562f698
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-06-25 13:35:53 -0500

    barbecue

---
+++ lua/plugins/barbecue.lua
@@ -0,0 +1,12 @@
+return {
+	"utilyre/barbecue.nvim",
+	dependencies = {
+		"SmiteshP/nvim-navic",
+		"nvim-tree/nvim-web-devicons", -- optional dependency
+	},
+	opts = {
+		show_dirname = false,
+		show_basename = false,
+		context_follow_icon_color = true,
+	},
+}
