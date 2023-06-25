d6c20195bb7ddf369a8cb6704429186104f46267
Lyle Cleveland <lcleveland@pop-os.localdomain>
2023-06-25 09:16:02 -0500

    cmp

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,53 @@
+return {
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"L3MON4D3/LuaSnip",
+	},
+	config = function()
+		local cmp = require("cmp")
+		cmp.setup({
+			mapping = cmp.mapping.preset.insert({
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+			}),
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+			}, {
+				{ name = "fuzzy_buffer" },
+			}),
+		})
+		cmp.setup.cmdline({ "/", "?" }, {
+			formatting = {
+				fields = { "abbr" },
+			},
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "fuzzy_buffer" },
+			},
+		})
+		cmp.setup.cmdline(":", {
+			formatting = {
+				fields = { "abbr" },
+			},
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "fuzzy_path" },
+			}, {
+				{ name = "cmdline" },
+			}),
+		})
+	end,
+}
