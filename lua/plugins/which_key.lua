ebe3a4e19968ea3a31213d16a7913ff9bc1c09b4
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-06-26 19:36:54 -0500

    settings

--- lua/plugins/which_key.lua
+++ lua/plugins/which_key.lua
@@ -61,5 +61,13 @@ return {
 				"Lsp hover doc",
 			},
 		})
+
+		-- codeium
+		vim.keymap.set("i", "<Tab>", function()
+			return vim.fn["codeium#Accept"]()
+		end, { expr = true })
+		vim.keymap.set("i", "<S-Tab>", function()
+			return vim.fn["codeium#CycleCompletions"](1)
+		end, { expr = true })
 	end,
 }
--- lua/post_setup.lua
+++ lua/post_setup.lua
@@ -1,9 +1,1 @@
 vim.opt.laststatus = 0
-
--- codeium keybinds
-vim.keymap.set("i", "<Tab>", function()
-	return vim.fn["codeium#Accept"]()
-end, { expr = true })
-vim.keymap.set("i", "<S-Tab>", function()
-	return vim.fn["codeium#CycleCompletions"](1)
-end, { expr = true })
